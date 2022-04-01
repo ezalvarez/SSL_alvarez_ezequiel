a6da21ce018135a338ec493de6045d58a085bf50
Ezequiel <82001137+liondrago@users.noreply.github.com>
2022-04-01 18:23:55 -0300

    Add files via upload

---
+++ 00-PrimerProgramaC/primerPrograma.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    printf("Materia: Sintaxis y Semántica de los Lenguajes \n");
+    printf("Curso: K2052\n");
+    printf("Profesor: Santiago Ferreiros Cabrera\n");
+    printf("Año: 2022\n");
+
+    system("pause");
+    return 0;
+}
